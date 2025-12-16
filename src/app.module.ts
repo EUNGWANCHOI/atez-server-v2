cfc53bb4d0d4e10be841b0e0e2ae2a52aa34c5b3
EUNGWANCHOI <silvergwan@gmail.com>
2025-12-16 16:55:09 +0900

    test2

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 //rename github
+//retest
 @Module({
   imports: [],
   controllers: [AppController],
